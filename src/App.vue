1844174a510f3436cbb59c489814474fa4db1a68
Waheed-Labib <labibwahid28@gmail.com>
2025-12-16 02:25:50 +0600

    theme and font configuration

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div >
+  <div class="space-mono-regular">
     <Navbar/>
     <router-view/>
   </div>
--- src/pages/Home/Hero/Hero.vue
+++ src/pages/Home/Hero/Hero.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="h-96 bg-yellow-600 flex items-center p-8">
-       Hero Section
+    <div class="h-96">
+       <div class="bg-(--primary) h-2/5">
+            
+       </div>
+
+       <div class="bg-(--background-low) h-3/5">   
+       </div>
+       
+       <div class="bg-(--background-high) h-1/5">
+       </div>
     </div>
 </template>
