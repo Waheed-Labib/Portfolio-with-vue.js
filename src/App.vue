800fc2fe1fd36b64e652905b73e1bbce9cdc7560
Waheed-Labib <labibwahid28@gmail.com>
2025-12-28 00:05:33 +0600

    added theme state to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
   import Navbar from "/src/components/Navbar/Navbar.vue"
+  import { ref } from "vue"
+  import type { Theme } from "./types"
+
+  const theme = ref<Theme>("midnight-neon-theme")
 </script>
 
 <template>
-  <div class="midnight-neon-theme space-mono-regular bg-(--background-high) min-h-screen">
+  <div :class="[theme, 'space-mono-regular bg-(--background-high) min-h-screen']">
     <Navbar/>
     <router-view/>
   </div>
---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type Theme = "midnight-neon-theme" | "honeymoon-suite-theme" | "forest-mist-theme"
